ab24a4feb74ca271877aac5523550f884a9e2bdd
Lucky <43052474+MrLuckyGamer@users.noreply.github.com>
2022-02-07 17:12:59 +1100

    Add files via upload

---
+++ loading/rules.php
@@ -0,0 +1,74 @@
+<?php
+
+$rules = [
+    
+    'cinema' => [
+        'Respect All Staff And Their Decisions!',
+        'Do not impersonate staff, or others!',
+        'Respect the players!',
+        'No verbal abuse!',
+        'No personal misuse(DOX)!',
+        'Please use english language, its disrespectful to others who dont speak your language.',
+        'Be moderate!',
+        'No earrape and NO mic/chat spam!',
+        'No human death or CP in any theaters!',
+        'No bullying, harrassing.'
+
+    ],
+
+    'dr' => [
+        'Press "F1" to learn about the gamemode',
+        'Press "F2" to open the Settings Menu',
+        'Press "F4" to open the Pointshop',
+        'Press "F7" to open the Jukebox',
+        'Press "F8" to toggle Third Person',
+        'Press "Q" to play your Pointshop Taunt',
+        'Press "C" to drop your Weapon',
+        'Type "!motd" to see Donator information, Forums, Staff list, Rules, etc.',
+        'Taking a while to load into our server? Subscribe to our workshop collection: https://steamcommunity.com/sharedfiles/filedetails/?id=1366316272'
+    ],
+    'bh' => [
+        'Press "F2" to Spectate',
+        'Press "F4" to open the Pointshop',
+        'Press "F7" to open the Jukebox',
+        'Press "Q" to play your Pointshop Taunt',
+        'Type "!r" to go back to the start of the map',
+        'Type "!style" to change your bunny hop style',
+        'Type "!motd" to see Donator information, Forums, Staff list, Rules, etc.',
+        'Type "!rtv" to call for a map vote. Use "!nominate" to pick a map that the RTV will include',
+        'Taking a while to load into our server? Subscribe to our workshop collection: https://steamcommunity.com/sharedfiles/filedetails/?id=1534462786'
+    ],
+    'murder' => [
+        'Press "Q" to Taunt',
+        'Press "F3" to play your Pointshop Taunt',
+        'Press "F4" to open the Pointshop',
+        'Press "F7" to open the Jukebox',
+        'Type "!3p" to toggle Third Person',
+        'Type "!steam" to join our Steam Group and receive 1,000 points and Member Rank',
+        'Type "!motd" to see Donator information, Forums, Staff list, Rules, etc.',
+        'Type "!rtv" in chat to call for a map vote.',
+        'Taking a while to load into our server? Subscribe to our workshop collection: https://steamcommunity.com/sharedfiles/filedetails/?id=1558832482'
+    ],
+    'ph' => [
+        'Press "F1" to learn about the gamemode',
+        'Press "F3" to open the Taunt Menu',
+        'Press "F4" to open the Pointshop',
+        'Press "F7" to open the Jukebox',
+        'Left Click/Mouse 1 to become a Prop',
+        'Press "C" to lock your prop rotation',
+        'Type "!3p" to toggle Third Person',
+        'Type "!motd" to see Donator information, Forums, Staff list, Rules, etc.',
+        'Taking a while to load into our server? Subscribe to our workshop collection: https://steamcommunity.com/sharedfiles/filedetails/?id=1469030254'
+    ],
+    'ttt' => [
+        'Press "F1" to open the Help and Settings Menu',
+        'Press "F3" to open the Weapon Selection Menu',
+        'Press "F4" to open the Pointshop',
+        'Press "F7" to open the Achievements Menu',
+        'Press "F8" to open the Damage Logs and RDM Manager',
+        'Press "F9" to play your Pointshop Taunt',
+        'Type "!motd" to see Donator information, Forums, Staff list, Rules, etc.',
+        'Type "!report" if you are RDMed/unfairly killed',
+        'Taking a while to load into our server? Subscribe to our workshop collection: https://steamcommunity.com/sharedfiles/filedetails/?id=1404060707'
+    ]
+];
